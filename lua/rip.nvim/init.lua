4a039233333eb4a3ab946b0f45700a8839a04aab
Lucas Pellegrinelli <lucas.pellegrinelli@hotmail.com>
2023-03-27 15:19:22 -0300

    Line nesting

--- lua/rip.nvim/init.lua
+++ lua/rip.nvim/init.lua
@@ -1,4 +1,13 @@
-local files = vim.fn.systemlist("git ls-files")
+local searchString = vim.fn.input("Search: ")
+
+local output = vim.fn.system("git ls-files | xargs grep -n -H -e '" .. searchString .. "'")
+local files = {}
+for line in string.gmatch(output, "[^\r\n]+") do
+  local file, line_number, match_text = string.match(line, "(.+):(%d+):(.+)")
+  if file then
+    table.insert(files, file .. ":" .. line_number .. ":" .. match_text)
+  end
+end
 
 -- Create the window
 local height = 15
@@ -17,7 +26,32 @@ local fileListOpts = {
 
 local fileListBuf = vim.api.nvim_create_buf(false, true)
 local fileListWin = vim.api.nvim_open_win(fileListBuf, true, fileListOpts)
-vim.api.nvim_buf_set_lines(fileListBuf, 0, -1, false, files)
+
+-- Build the tree
+local tree = {}
+for _, entry in ipairs(files) do
+  local file, line_number, match_text = string.match(entry, "(.+):(%d+):(.+)")
+  if file and line_number and match_text then
+    if not tree[file] then
+      tree[file] = {}
+    end
+    tree[file][line_number .. ":" .. match_text] = ""
+  end
+end
+
+-- Add the entries to the buffer recursively
+local function add_entries(node, indent)
+  for key, value in pairs(node) do
+    local line = string.rep(" ", indent) .. key
+    vim.api.nvim_buf_set_lines(fileListBuf, -1, -1, false, {line})
+    if type(value) == "table" then
+      add_entries(value, indent + 2)
+    end
+  end
+end
+
+add_entries(tree, 0)
+
 vim.api.nvim_buf_set_option(fileListBuf, "modifiable", false)
 
 -- Define ToggleMark function to toggle marking the selected file name with an asterisk
